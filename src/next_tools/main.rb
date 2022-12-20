9ef48e4218e368e6541f2ece45969698117a4258
duke from earth <stephen_duke@live.com>
2022-12-19 21:18:44 -0500

    intialize variables and pass through references to the observers

--- src/next_tools/main.rb
+++ src/next_tools/main.rb
@@ -1,9 +1,10 @@
-require "next_tools/tool_observer.rb"
-require "next_tools/app_observer.rb"
+require "next_tools/tool_observer"
+require "next_tools/app_observer"
+require "next_tools/file_handler"
 
 module NextTool
-  # SKETCHUP_CONSOLE.show
-
+  SKETCHUP_CONSOLE.show
+  file_name = "NextTool"
   # Add items to menu
   plugins_menu = UI.menu("Plugins")
   submenu = plugins_menu.add_submenu("Next Tools")
@@ -14,7 +15,11 @@ module NextTool
     MyToolsObserver.clear_tools()
   }
 
+  previous_tools = FileHandler.read_json_to_hash(file_name)
+  tools_observer = MyToolsObserver.new(previous_tools)
+  app_observer = MyAppObserver.new(tools_observer, file_name)
+
   # Attach the observers
-  Sketchup.add_observer(MyAppObserver.new)
-  Sketchup.active_model.tools.add_observer(MyToolsObserver.new)
+  Sketchup.add_observer(app_observer)
+  Sketchup.active_model.tools.add_observer(tools_observer)
 end
