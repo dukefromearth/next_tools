27a3600a3429cf42fc414c3c6c23ccdd851048f6
duke from earth <stephen_duke@live.com>
2022-12-19 21:17:17 -0500

    refactor

--- src/next_tools/app_observer.rb
+++ src/next_tools/app_observer.rb
@@ -1,8 +1,14 @@
-require "next_tools/tool_observer"
+require "next_tools/file_handler"
 
 # Saves the gathered data to a json file on quit
 class MyAppObserver < Sketchup::AppObserver
+  def initialize(observer, file_name)
+    @tools_observer = observer
+    @file_name = file_name
+  end
+
   def onQuit()
-    MyToolsObserver.save_tools()
+    puts @file_name
+    FileHandler.save_json(@tools_observer.get_tools, @file_name)
   end
 end
